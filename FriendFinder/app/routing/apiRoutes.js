51c7a3fac30ef3facf43a102214cbc9e55cb7a92
Dennis The Menace <sdennisp25@hotmail.com>
2018-10-31 23:39:29 -0600

    obtained value from survey

--- FriendFinder/app/routing/apiRoutes.js
+++ FriendFinder/app/routing/apiRoutes.js
@@ -9,5 +9,37 @@ module.exports = function (app) {
 
 	app.post('/api/friends', function (req, res) {
 
+		var greatMatch = {
+			name: "",
+			image: "",
+			matchDifference: 1000
+		};
+		var usrData = req.body;
+		var usrName = usrData.name;
+		var usrImage = usrData.image;
+		var usrScores = usrData.scores;
+
+		var totalDifference = 0;
+
+		//loops through the friendsList array
+		for (var i = 0; i < friendsList.length - 1; i++) {
+			console.log(friendsList[i].name);
+			totalDifference = 0;
+
+			for (var j = 0; j < 10; j++) {
+
+				totalDifference += Math.abs(parseInt(usrScores[j]) - parseInt(friendsList[i].scores[j]));
+
+				if (totalDifference <= greatMatch.friendDifference) {
+					greatMatch.name = friendsList[i].name;
+					greatMatch.photo = friendsList[i].photo;
+					greatMatch.matchDifference = totalDifference;
+				}
+			}
+		}
+
+		friendsList.push(usrData);
+
+		res.json(greatMatch);
 	});
 };
